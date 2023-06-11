bc1d965ffc70a3377d3e1608751a6b6823ae62e0
PARKEUNGYEONG <eungyeong1140@naver.com>
2023-06-11 21:52:20 +0900

    practice

--- src/main/java/org/zerock/workbook/todo/TodoRegisterController.java
+++ src/main/java/org/zerock/workbook/todo/TodoRegisterController.java
@@ -1,6 +1,7 @@
 package org.zerock.workbook.todo;
 //TodoRegisterController은 특이하게 GET방식으로 호출되는 경우에는 입력할 수 있는 화면을 보여주고 POST방식으로 호출되는 경우에는 등록이 처리된 후에 다시목페이지(/to do/list)를 호출하게 된다(sendRediresct()).
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,10 +15,17 @@ public class TodoRegisterController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException{
         System.out.println("입력 화면을 볼 수 있도록 구성");
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/todo/register.jsp");
+
+        dispatcher.forward(req,resp);
     }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)throws ServletException,IOException{
         System.out.println("입력을 처리하고 목록 페이지로 이동");
+
+        //브라우저가 호출해야 하는 주소
+        resp.sendRedirect("/todo/list");
     }
 
 }
---
+++ src/main/webapp/WEB-INF/todo/register.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: seongjun
+  Date: 2023/06/11
+  Time: 8:58 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form action="/todo/register" method="post"><!--action과 method의 속성을 이용해서 화면상의 버튼을 클릭하면 post 방식으로 todoREgisterController을 호출하도록 구성한다.-->
+    <button type="submit">등록처리</button>
+</form>
+
+</body>
+</html>
