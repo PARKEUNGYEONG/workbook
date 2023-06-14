33d4a95742f4c5dae42e410ce5353016be18e140
PARKEUNGYEONG <eungyeong1140@naver.com>
2023-06-14 21:39:18 +0900

    practice

--- src/main/java/org/zerock/workbook/todo/service/TodoService.java
+++ src/main/java/org/zerock/workbook/todo/service/TodoService.java
@@ -10,16 +10,14 @@ import java.util.stream.IntStream;
 
 public enum TodoService {
     INSTANCE;//객채의 개수를 결정하는 부분
-
-    public void register(TodoDTO todoDTO){
-
+    public void register(TodoDTO todoDTO){//register()의 경우 새로운 TodoDTO 객체를 받아서 확인할수 있는것을 목표로 작성
         System.out.println("DEBUG....."+todoDTO);
 
     }
 
     public List<TodoDTO> getList(){
 
-        List<TodoDTO>todoDTOS = IntStream.range(0,10).mapToObj(i ->{
+        List<TodoDTO>todoDTOS = IntStream.range(0,10).mapToObj(i ->{//getList()는 10개의 TodoDTO객체를 만들어서 반환하도록 구성
         TodoDTO dto = new TodoDTO();
         dto.setTno((long)i);
         dto.setTitle("Todo.."+i);
